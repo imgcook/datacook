f63d63ae223338b7896c236c3dbf4d739014cddd
steveoni <steohenoni2@gmail.com>
2020-11-04 16:04:36 +0100

    [eslint] fix eslint error

--- src/image/image-proc.ts
+++ src/image/image-proc.ts
@@ -1,5 +1,5 @@
 import Jimp from 'jimp';
-import {img2array,tensor2Img,Tensor2ImgArgs, std} from './utils';
+import { img2array, tensor2Img, Tensor2ImgArgs, std } from './utils';
 import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-cpu';
 
@@ -10,7 +10,7 @@ import '@tensorflow/tfjs-backend-cpu';
 export default class Image {
   private img: Jimp; //img accessible to all methods in the class
 
-  constructor (data: Jimp){  
+  constructor (data: Jimp){
     this.img = data;
   }
 
@@ -19,29 +19,29 @@ export default class Image {
    * @param name image file name
    */
   static async read(name: string): Promise<Image>{
-        
+
     const jimp_obj = await Jimp.read(name);
-  
+
     return new Image(jimp_obj);
-    }
+  }
 
   /**
    * Convert image Buffer to Tensor
    * @param depth -> number of channel to obtain from image
    * @return Promise<Array<Array<Array<number>>>> -> A 3dimensional array
    */
-  public img2tensor(depth: number=3): tf.Tensor3D{
+  public img2tensor(depth = 3): tf.Tensor3D{
 
     const tensor = img2array({
       data: this.data,
       height: this.height,
       width: this.width,
       channel: 4,
-      new_channel: depth,
+      new_channel: depth
     });
-  
-    return tensor
-  } 
+
+    return tensor;
+  }
 
   /**
    * Convert Image Tensor to one dimensional Array to be converted to Buffer.
@@ -52,19 +52,19 @@ export default class Image {
     const img_array = tensor2Img(opt);
     const data: Buffer = Buffer.from(img_array);
 
-    const img: Jimp = await new Jimp({data:data, width:opt.width, height:opt.height});
+    const img: Jimp = await new Jimp({ data:data, width:opt.width, height:opt.height });
     return new Image(img);
   }
-  
+
   /**
    * Resize image
    * @param width number
    * @param height number
-   * @return Image 
+   * @return Image
    */
   public resize(width:number, height:number): Image {
-    this.img = this.img.resize(width,height);
-  
+    this.img = this.img.resize(width, height);
+
     return this;
   }
 
@@ -85,10 +85,10 @@ export default class Image {
    * @param name the image file name
    * @return Boolean
    */
-  public save_img(name:string): Boolean{
-    
+  public save_img(name:string): boolean{
+
     const is_save = this.img.write(name);
-    if(is_save){
+    if (is_save){
       return true;
     }
     return false;
@@ -106,54 +106,54 @@ export default class Image {
 
   /**
    * flip the image to horizontal or vertical
-   * @param horz 
-   * @param vert 
+   * @param horz
+   * @param vert
    * @return Image
    */
   public flip(horz: boolean, vert: boolean): Image{
-    this.img = this.img.flip(horz,vert);
+    this.img = this.img.flip(horz, vert);
     return this;
   }
 
   /**
    * Crop the desired side of an image
-   * @param x 
-   * @param y 
-   * @param width 
-   * @param height 
+   * @param x
+   * @param y
+   * @param width
+   * @param height
    * @return Image
    */
   public crop(x:number, y:number, width:number, height:number): Image{
-    this.img = this.img.crop(x,y,width, height);
+    this.img = this.img.crop(x, y, width, height);
     return this;
   }
 
   /**
    * Normalize images to [-1,1] using (tensor - mean) / std
-   * @param data 
-   * @param mean 
-   * @param im_std 
+   * @param data
+   * @param mean
+   * @param im_std
    * @return Tensor3D
    */
-  static normalize(data: tf.Tensor3D, mean?: number,im_std?:number): tf.Tensor3D{
+  static normalize(data: tf.Tensor3D, mean?: number, im_std?:number): tf.Tensor3D{
 
     const tf_mean = mean ? mean : data.mean().round().arraySync();
     const tf_std = im_std ? im_std : std(data).round().arraySync();
     const norm = data.sub(tf_mean).div(tf_std);
 
-    return norm as tf.Tensor3D
+    return norm as tf.Tensor3D;
   }
 
   /**
    * Un-normalize a normalize image tensor
-   * @param data 
-   * @param mean 
-   * @param tf_std 
+   * @param data
+   * @param mean
+   * @param tf_std
    * @returns Tensor
    */
-  static unnormalize(data: tf.Tensor3D, mean: number,tf_std:number): tf.Tensor3D{
+  static unnormalize(data: tf.Tensor3D, mean: number, tf_std:number): tf.Tensor3D{
     const unnorm = tf.cast(data.mul(tf_std).add(mean), "int32");
-    return unnorm as tf.Tensor3D
+    return unnorm as tf.Tensor3D;
   }
 
 }
--- src/image/utils.ts
+++ src/image/utils.ts
@@ -20,14 +20,14 @@ export function img2array(opt: Img2TensorArgs): tf.Tensor3D {
 
   let img_array: Array<Array<Array<number>>> = [];
 
-  for(let ch=0; ch < opt.new_channel;ch++){
+  for (let ch = 0; ch < opt.new_channel;ch++){
     let channel_array: Array<Array<number>> = [];
 
-    for(let j=0; j < opt.width; j++){
+    for (let j = 0; j < opt.width; j++){
       let inner_array: Array<number> = [];
 
-      for(let k=0; k < opt.height; k++){
-        let index: number = ((opt.width*k)+j)*opt.channel +ch;
+      for (let k = 0; k < opt.height; k++){
+        let index: number = ((opt.width * k) + j) * opt.channel + ch;
         inner_array.push(opt.data[index]);
       }
       channel_array.push(inner_array);
@@ -39,33 +39,33 @@ export function img2array(opt: Img2TensorArgs): tf.Tensor3D {
 
 export function tensor2Img(opt: Tensor2ImgArgs): Array<number>{
 
-  let new_array: Array<number> = new Array(opt.width*
-      opt.height*opt.channel);
-  
-  let data = opt.data.arraySync()
+  let new_array: Array<number> = new Array(opt.width *
+      opt.height * opt.channel);
+
+  let data = opt.data.arraySync();
   let channel_array: Array<Array<number>> = [];
 
-  for(let ch=0; ch < opt.channel; ch++){
+  for (let ch = 0; ch < opt.channel; ch++){
     channel_array = data[ch];
 
-    for(let j=0; j < opt.width; j++){
-        for(let k=0; k< opt.height; k++){
-          let index: number  = ((opt.width*k)+j)*opt.channel +ch;
-          new_array[index] = channel_array[j][k];
-        }
+    for (let j = 0; j < opt.width; j++){
+      for (let k = 0; k < opt.height; k++){
+        let index: number = ((opt.width * k) + j) * opt.channel + ch;
+        new_array[index] = channel_array[j][k];
+      }
     }
   }
   return new_array;
 }
 
-export function std(data: tf.Tensor3D) {
+export function std(data: tf.Tensor3D): tf.Tensor {
 
-  let tensor_data = data
-  
-  let mean = tensor_data.mean()
-  let sub_mean_pow = tensor_data.sub(mean).pow(2)
-  let mean_data = sub_mean_pow.mean()
-  let std = mean_data.sqrt()
-  
-  return std
+  let tensor_data = data;
+
+  let mean = tensor_data.mean();
+  let sub_mean_pow = tensor_data.sub(mean).pow(2);
+  let mean_data = sub_mean_pow.mean();
+  let std = mean_data.sqrt();
+
+  return std;
 }
