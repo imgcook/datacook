a4897816e41394814cd4669cb215fa534ee39c61
steveoni <steohenoni2@gmail.com>
2020-12-06 20:07:54 +0100

    [word2vec][s]: add utils functions for word2vec

---
+++ src/text/word2vec-utils.ts
@@ -0,0 +1,90 @@
+export function clean(text: string, stopwords: string[] | undefined): Array<string> {
+  let textArray = text.split(' ');
+  //filter out empty string
+  textArray = textArray.filter((value) => {
+    return value != '';
+  });
+
+  //filter out stopwords.
+  if (stopwords) {
+    textArray = textArray.filter((value) => {
+      return !stopwords.includes(value);
+    });
+  }
+  //remove punctuation
+  textArray = textArray.map((value) => {
+    return value.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  });
+
+  return textArray;
+}
+
+export function skipGram(
+  window = 5, textArray: Array<string>,
+  stopwords: string[] | undefined): Array<Array<string | string[]>> {
+
+  let wordList = [];
+  let allText = [];
+
+  for (const index in textArray) {
+    let text = textArray[index];
+    let textClean = clean(text, stopwords);
+
+    allText.push(...textClean); //store clean text
+    for (let i = 0; i < textClean.length; i++) {
+      let word = textClean[i];
+      for (let j = 0; j < window; j++) {
+        //skip-gram forward
+        if ((i + 1 + j) < textClean.length) {
+          wordList.push([ word, textClean[i + 1 + j] ]);
+        }
+        //skip-gram backward
+        if ((i - j - 1) >= 0) {
+          wordList.push([ word, textClean[i - j - 1] ]);
+        }
+      }
+    }
+  }
+  return [ wordList, allText ];
+}
+
+interface UniqueWord {
+  [word: string] : number;
+}
+
+export function createUniqueWord(textArray: string[]): UniqueWord {
+  const wordSet = new Set(textArray);
+  const wordList = Array.from(wordSet);
+  let uniqueWords: UniqueWord = {};
+
+  for (let i = 0; i < wordList.length; i++){
+    const word = wordList[i];
+    uniqueWords[word] = i + 1;
+  }
+  return uniqueWords;
+}
+
+export function objectLength(dict: UniqueWord): number {
+  return Object.keys(dict).length;
+}
+
+export function createData(
+  wordList: string[][], uniqueWords: UniqueWord,
+  nWords: number): Array<Array<number | Float64Array>> {
+
+  let data = [];
+  let label = [];
+
+  for (let i = 0; i < wordList.length; i++) {
+    const [ x, y ] = wordList[i];
+    const wordIndex = uniqueWords[x];
+    const contextIndex = uniqueWords[y];
+
+    let xRow = new Float64Array(nWords);
+    xRow[wordIndex] = 1;
+    data.push(xRow);
+    label.push(contextIndex);
+  }
+
+  return [ data, label ];
+}
---
+++ test/text/word2vec-utils.ts
@@ -0,0 +1,103 @@
+import { assert, expect } from "chai"
+import { createData, createUniqueWord, objectLength, skipGram } from '../../src/text/word2vec-utils';
+import { text_list, stopwords, text_lower, text} from './global';
+
+describe("Word2Vec Utils", () => {
+  
+  it("should generate skip gram using window =5", () => {
+    
+    const [ wordList, allText ] = skipGram(5, text_list, stopwords);
+    const expectedWordList = [
+                              [ 'king', 'man' ],
+                              [ 'king', 'rules' ],
+                              [ 'king', 'over' ],
+                              [ 'king', 'nation' ],
+                              [ 'king', 'he' ],
+                              [ 'man', 'rules' ],
+                              [ 'man', 'king' ],
+                              [ 'man', 'over' ],
+                              [ 'man', 'nation' ],
+                              [ 'man', 'he' ]
+                            ]
+    const expectedWordListLength = 190;
+    const expectedAllText = [
+                              'king',      'man',      'rules',
+                              'over',      'nation',   'he',
+                              'always',    'have',     'woman',
+                              'beside',    'him',      'called',
+                              'queen',     'she',      'helps',
+                              'king',      'controls', 'affars',
+                              'nation',    'perhaps',  'she',
+                              'acclaimed', 'position', 'king',
+                              'king',      'her',      'husband',
+                              'deceased'
+                            ]
+
+    assert.deepEqual(wordList.slice(0,10), expectedWordList);
+    assert.deepEqual(wordList.length, expectedWordListLength);
+    assert.deepEqual(allText, expectedAllText);
+  });
+
+  it('should obtain number of element in an object', () => {
+    const obj = {
+      "King": 2,
+      "Queen": 4,
+      "Lady": 6,
+      "woman": 8,
+      "gh": 9
+    }
+    const length = 5;
+    assert.deepEqual(objectLength(obj), length);
+  });
+
+  it("should generate Unique word dictionary", () => {
+    const textList = [
+                      'king',      'man',      'rules',
+                      'over',      'nation',   'he',
+                      'always',    'have',     'woman',
+                      'beside',    'him',      'called',
+                      'queen',     'she',      'helps',
+                      'king',      'controls', 'affars',
+                      'nation',    'perhaps',  'she',
+                      'acclaimed', 'position', 'king',
+                      'king',      'her',      'husband',
+                      'deceased'
+                    ]
+    const uniqueWords = {
+                          king: 1,
+                          man: 2,
+                          rules: 3,
+                          over: 4,
+                          nation: 5,
+                          he: 6,
+                          always: 7,
+                          have: 8,
+                          woman: 9,
+                          beside: 10,
+                          him: 11,
+                          called: 12,
+                          queen: 13,
+                          she: 14,
+                          helps: 15,
+                          controls: 16,
+                          affars: 17,
+                          perhaps: 18,
+                          acclaimed: 19,
+                          position: 20,
+                          her: 21,
+                          husband: 22,
+                          deceased: 23
+                        }
+    assert.deepEqual(createUniqueWord(textList), uniqueWords);
+  });
+
+  it("should generate dataset for word2vec", () => {
+    const [ wordList, allText ] = skipGram(5, text_list, stopwords);
+    const uniqueWords = createUniqueWord(allText as string[]);
+    const nwords = objectLength(uniqueWords);
+    const [ data, label ] = createData(wordList as string[][], uniqueWords, nwords);
+
+    assert.equal(data.length, 190);
+    assert.equal(label.length, 190);
+  });
+});
