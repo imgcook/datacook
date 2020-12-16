a35c3a4d33062d0a5747f91db80cd8018be52010
Rising Odegua <risingodegua@gmail.com>
2020-12-16 08:10:45 +0100

    [webpack.config][s]: Fix quote styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const path = require('path');
+const path = require("path");
 
 const createConfig = (target) => {
   return {
-    mode: 'production',
-    devtool: 'source-map',
+    mode: "production",
+    devtool: "source-map",
     context: path.resolve(__dirname),
     entry: {
       index: `./dist/index.js`
     },
     target: target,
     output: {
-      path: path.resolve(__dirname, 'dist/browser'),
+      path: path.resolve(__dirname, "dist/browser"),
       filename: `bundle.js`,
-      library: 'datacook'
+      library: "datacook"
     },
     module: {
       rules: [
         {
           use: {
-            loader: 'babel-loader',
-            options: { presets: [ '@babel/preset-env' ] }
+            loader: "babel-loader",
+            options: { presets: [ "@babel/preset-env" ] }
           },
           test: /\.(js|jsx)$/,
           exclude: /node_modules/
         }
       ]
     },
-    node: { fs: 'empty' }
+    node: { fs: "empty" }
   };
 };
 
 module.exports = [
-  createConfig('web')
+  createConfig("web")
 ];
