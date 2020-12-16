7423e4e91ff0da8b47077a2d1f6eddd904994f8a
Rising Odegua <risingodegua@gmail.com>
2020-12-16 10:01:36 +0100

    Add Browser test configuration and scripts (#10)
    * [browser-test][m]: Add test configurations for browser env
    
    * [browser-test][m]: Add test scripts for browser env
    
    * [test folders][s]: Update folder structure and ReadMe
    
    * [browser-test][m]: Change testing framework to mocha and chai
    
    * [package.json][s]: Update test script
    
    * [webpack.config][s]: Fix quote styling
    
    * [gitignore[s]: Add yarn error log to gitignore

--- test/node/image/image-proc.ts
+++ test/node/image/image-proc.ts
@@ -1,6 +1,6 @@
 import { assert, expect } from "chai"
-import Image from "../../src/image/image-proc";
-import {stdCalc} from "../../src/image/utils";
+import Image from "../../../src/image/image-proc";
+import {stdCalc} from "../../../src/image/utils";
 import * as fs from 'fs';
 import '@tensorflow/tfjs-backend-cpu';
 
@@ -8,43 +8,43 @@ describe("Image-proc", ()=>{
 
   it("should resize image properly", async ()=>{
 
-    const img: Image = await Image.read("test/image/artifacts/dog.jpg")
+    const img: Image = await Image.read("test/node/image/artifacts/dog.jpg")
     const resizeData = [...img.resize(100,100).data];
-    const expectedData = [...fs.readFileSync("test/image/artifacts/resize100.ds")];
+    const expectedData = [...fs.readFileSync("test/node/image/artifacts/resize100.ds")];
     assert.deepEqual(resizeData,expectedData);
   });
   it("should convert image to tensor", async ()=>{
 
-    const img: Image = await Image.read("test/image/artifacts/dog.jpg");
+    const img: Image = await Image.read("test/node/image/artifacts/dog.jpg");
     const resizeTensor = img.resize(50,50).toTensor();
-    const expectedTensor = new Int32Array([...fs.readFileSync("test/image/artifacts/resize50_tensor.ds")]); 
+    const expectedTensor = new Int32Array([...fs.readFileSync("test/node/image/artifacts/resize50_tensor.ds")]); 
     assert.deepEqual(resizeTensor.dataSync(),expectedTensor);
   });
   it("should convert tensor3D back to image", async ()=>{
 
-    const img: Image = await Image.read("test/image/artifacts/dog.jpg");
+    const img: Image = await Image.read("test/node/image/artifacts/dog.jpg");
 
     const resizeTensor = img.resize(50,50).toTensor();
     const resizedArray  = await Image.fromTensor(resizeTensor);
 
     const resizeData = [...resizedArray.data];
-    const expectedData = [...fs.readFileSync("test/image/artifacts/tensor2img50.ds")];
+    const expectedData = [...fs.readFileSync("test/node/image/artifacts/tensor2img50.ds")];
 
     assert.deepEqual(resizeData,expectedData);
   });
   it("should write image to a file", async ()=>{
 
-    const img: Image = await Image.read("test/image/artifacts/dog.jpg"); 
+    const img: Image = await Image.read("test/node/image/artifacts/dog.jpg"); 
     const resizeImg = img.resize(50,50);
-    const isSave = resizeImg.save("test/image/artifacts/img_resize.jpg")
+    const isSave = resizeImg.save("test/node/image/artifacts/img_resize.jpg")
 
-    fs.unlinkSync("test/image/artifacts/img_resize.jpg");
+    fs.unlinkSync("test/node/image/artifacts/img_resize.jpg");
     expect(isSave).to.be.true;
   });
 
   it("should convert all image procesing to tensor", async ()=>{
 
-    const img: Image = await Image.read("test/image/artifacts/dog.jpg"); 
+    const img: Image = await Image.read("test/node/image/artifacts/dog.jpg"); 
 
     const imgTensor = img.resize(300,300)
                           .crop(0,0,200,200)
@@ -52,13 +52,13 @@ describe("Image-proc", ()=>{
                           .rotate(90)
                           .toTensor()
 
-    const expectedTensor = new Int32Array([...fs.readFileSync("test/image/artifacts/img_chain_tensor.ds")]);
+    const expectedTensor = new Int32Array([...fs.readFileSync("test/node/image/artifacts/img_chain_tensor.ds")]);
     assert.deepEqual(imgTensor.dataSync(),expectedTensor);
   });
 
   it("should normalize and unormalize an image tensor", async ()=>{
 
-    const img: Image = await Image.read("test/image/artifacts/dog.jpg");
+    const img: Image = await Image.read("test/node/image/artifacts/dog.jpg");
 
     const resizeTensor = img.resize(50,50).toTensor();
     const img2: Image = await Image.fromTensor(resizeTensor);
