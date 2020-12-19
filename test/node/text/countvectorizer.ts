7c9d75d1ef298cad7a2afa8e9d31b59dca47016e
steveoni <steohenoni2@gmail.com>
2020-12-19 19:28:01 +0100

    [countvectorizer][s]: add counter to /test/node/text

---
+++ test/node/text/countvectorizer.ts
@@ -0,0 +1,26 @@
+import { assert } from 'chai';
+import CountVectorizer from "../../../src/text/countvectorizer";
+
+describe('CountVectorizer', function(){
+  it('should convert words to their proper countvectorizer', function(){
+    const vectorizer = new CountVectorizer(["The quick brown fox jumped over the lazy dog."]);
+    const toVector = vectorizer.transform(["the brown brown","the the brown"]);
+    const vocab = {
+                    brown: 0,
+                    dog: 1,
+                    fox: 2,
+                    jumped: 3,
+                    lazy: 4,
+                    over: 5,
+                    quick: 6,
+                    the: 7
+                  }
+    const expectedVector = [
+                            [2, 0, 0, 0,0, 0, 0, 1],
+                            [1, 0, 0, 0,0, 0, 0, 2]
+                           ]
+
+    assert.deepEqual(vectorizer.wordOrder, vocab);
+    assert.deepEqual(toVector, expectedVector);
+  });
+});
