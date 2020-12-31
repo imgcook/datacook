3d41ec43795c563787de2aee5446eeb1b5471dbc
Yorkie Liu <yorkie.ninja.1990@gmail.com>
2020-12-31 12:53:40 +0800

    doc: add sections in readme (#19)
    * doc: add sections in readme

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import '@tensorflow/tfjs-backend-cpu';
 
+import Counter from './text/counter';
+import CountVectorizer from './text/countvectorizer';
+import Word2Vec from './text/word2vec';
+
 export * as Generic from './generic';
+export * as Image from './image/image-proc';
+export const Text = {
+  Counter,
+  CountVectorizer,
+  Word2Vec
+};
