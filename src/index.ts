5818d10b36fbe53fe92302e4af6deb53c0100489
Rising Odegua <risingodegua@gmail.com>
2021-01-12 22:13:43 +0100

    [index]: Export encoding functions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import '@tensorflow/tfjs-backend-cpu';
+import "@tensorflow/tfjs-backend-cpu";
 
-import Counter from './text/counter';
-import CountVectorizer from './text/countvectorizer';
-import Word2Vec from './text/word2vec';
+import Counter from "./text/counter";
+import CountVectorizer from "./text/countvectorizer";
+import Word2Vec from "./text/word2vec";
+import { LabelEncoder, OneHotEncoder } from "./tabular/encoder";
 
-export * as Generic from './generic';
-export * as Image from './image/image-proc';
+
+export * as Generic from "./generic";
+export * as Image from "./image/image-proc";
+
 export const Text = {
   Counter,
   CountVectorizer,
   Word2Vec
+};
+
+export const Encoder = {
+  LabelEncoder,
+  OneHotEncoder
 };
