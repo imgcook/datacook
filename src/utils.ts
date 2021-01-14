8e0980b7f3eede2bf966901a21fa49cca9e39c66
Rising Odegua <risingodegua@gmail.com>
2021-01-14 11:04:01 +0100

    [utils][s]: optimize functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,20 @@
-import { Tensor, Tensor1D } from "@tensorflow/tfjs-core";
+import { Tensor, Tensor1D } from '@tensorflow/tfjs-core';
 
 function _zeros(row: number, column: number): Array<any> {
   let zero_array = [];
-
   for (let i = 0; i < row; i++) {
-    let col_data = Array(column);
-    for (let j = 0; j < column; j++) {
-      col_data[j] = 0;
-    }
+    let col_data = Array.from(new Uint8Array(column));
     zero_array.push(col_data);
   }
   return zero_array;
 }
 
 
-function getDataByType(data: Tensor1D[] | Array<any> | any) : Array<any>{
+async function getDataByType(data: Tensor1D[] | Array<any> | any) : Promise<any>{
   if (Array.isArray(data)) {
     return data;
   } else if (data instanceof Tensor) {
-    return data.arraySync();
+    return await data.array();
   } else {
     return undefined;
   }
