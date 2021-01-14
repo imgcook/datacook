835c6a356568ecc2d876be7403bdb979e4ee6f7f
Rising Odegua <risingodegua@gmail.com>
2021-01-14 11:03:40 +0100

    [index][s]: Updates double to single quotes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import "@tensorflow/tfjs-backend-cpu";
+import '@tensorflow/tfjs-backend-cpu';
 
-import Counter from "./text/counter";
-import CountVectorizer from "./text/countvectorizer";
-import Word2Vec from "./text/word2vec";
-import { LabelEncoder, OneHotEncoder } from "./tabular/encoder";
+import Counter from './text/counter';
+import CountVectorizer from './text/countvectorizer';
+import Word2Vec from './text/word2vec';
+import { LabelEncoder, OneHotEncoder } from './tabular/encoder';
 
 
-export * as Generic from "./generic";
-export * as Image from "./image/image-proc";
+export * as Generic from './generic';
+export * as Image from './image/image-proc';
 
 export const Text = {
   Counter,
