440b27ca48b1dc13aae4813ea0e6b451351c90bd
eric.lwh <eric.lwh@alibaba-inc.com>
2021-02-01 11:51:04 +0800

    add description

--- src/rand/beta.ts
+++ src/rand/beta.ts
@@ -1,7 +1,20 @@
 export declare class Beta {
-  constructor(a?: number, b?:number)
+  /**
+   * New a Beta Distribution Generator
+   * @param a 
+   * @param b 
+   */
+  constructor(a?: number, b?:number);
 
+  /**
+   * Sample a value from Beta Distribution
+   */
   generate(): number;
 
+  /**
+   * Set a new set of a & b param
+   * @param a Alpha
+   * @param b Beta
+   */
   setParam(a: number, b: number): void;
 }
