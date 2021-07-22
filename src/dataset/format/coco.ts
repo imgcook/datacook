ed9bf937eb350c2ea6dcb5c4a8e1081dda73f3cc
FeelyChau <feely@outlook.com>
2021-07-22 19:12:17 +0800

    fix

---
+++ src/dataset/format/coco.ts
@@ -0,0 +1,80 @@
+import { Dataset, Sample, DatasetType, Coco } from '../types';
+import { DatasetData, makeDataset } from '../utils';
+
+async function checkCocoMeta(metaObj: Record<string, any>) {
+  if (!Array.isArray(metaObj.images)) {
+    throw new TypeError('images should be array');
+  }
+  if (!Array.isArray(metaObj.annotations)) {
+    throw new TypeError('annotations should be array');
+  }
+  metaObj.images.forEach((image) => {
+    if (typeof image.id !== 'number') {
+      throw new TypeError('invalid id field found in image data');
+    }
+    if (typeof image.width !== 'number') {
+      throw new TypeError('invalid width field found in image data');
+    }
+    if (typeof image.height !== 'number') {
+      throw new TypeError('invalid height field found in image data');
+    }
+    if (
+      typeof image.url !== 'string'
+      && typeof image.coco_url !== 'string'
+      && typeof image.flickr_url !== 'string'
+    ) {
+      throw new TypeError('invalid url/flickr_url/coco_url field found in image data');
+    }
+  });
+}
+
+function cocoMetaToDatasetData(cocoMeta: Coco.Meta): Array<Sample<Coco.Image, Coco.Label>> {
+  const annotationMap: Record<number, Array<Coco.Annotation>> = {};
+  for (const ann of cocoMeta.annotations) {
+    if (!annotationMap[ann.image_id]) {
+      annotationMap[ann.image_id] = [];
+    }
+    annotationMap[ann.image_id].push(ann);
+  }
+  return cocoMeta.images.map((img: Coco.Image) => ({ data: img, label: annotationMap[img.id] }));
+}
+
+async function process(
+  annotationObj: Coco.Meta
+): Promise<{
+  meta: Coco.Meta,
+  datasetData: Array<Sample<Coco.Image, Coco.Label>>
+}> {
+  await checkCocoMeta(annotationObj);
+  return { meta: annotationObj as Coco.Meta, datasetData: cocoMetaToDatasetData(annotationObj) };
+}
+
+export const makeDatasetFromCocoFormat = async (options: Coco.Options): Promise<Dataset<Sample<Coco.Image, Coco.Label>, Coco.DatasetMeta>> => {
+  const { meta: trainMeta, datasetData: trainDatasetData } = await process(options.trainAnnotationObj);
+  const { datasetData: testDatasetData } = await process(options.testAnnotationObj);
+  let validDatasetData = undefined;
+  if (options.validAnnotationObj) {
+    validDatasetData = (await process(options.validAnnotationObj)).datasetData;
+  }
+  const data: DatasetData<Sample<Coco.Image, Coco.Label>> = {
+    trainData: trainDatasetData,
+    testData: testDatasetData,
+    validData: validDatasetData
+  };
+  const labelMap: Record<number, Coco.Category> = {};
+  (trainMeta as Coco.Meta).categories.forEach((category: Coco.Category) => {
+    labelMap[category.id] = category;
+  });
+  const datasetMeta: Coco.DatasetMeta = {
+    type: DatasetType.Image,
+    size: {
+      train: trainDatasetData.length,
+      test: testDatasetData.length,
+      valid: Array.isArray(validDatasetData) ? validDatasetData.length : 0
+    },
+    labelMap,
+    info: trainMeta.info,
+    licenses: trainMeta.licenses
+  };
+  return makeDataset(data, datasetMeta);
+};
---
+++ src/dataset/object-detection.ts
@@ -0,0 +1,32 @@
+import { ObjectDetection, Coco, Sample } from './types';
+import { makeDatasetFromCocoFormat } from './format/coco';
+import { transformDataset } from './';
+
+export const makeObjectDetectionDatasetFromCoco = async (options: Coco.Options): Promise<ObjectDetection.Dataset> => {
+  const dataset = await makeDatasetFromCocoFormat(options);
+  return transformDataset<Coco.DatasetMeta, Sample<Coco.Image, Coco.Label>, ObjectDetection.Sample, ObjectDetection.DatasetMeta>({
+    next: async (sample: Sample<Coco.Image, Coco.Label>): Promise<ObjectDetection.Sample> => {
+      const newLabels = sample.label.map((lable) => {
+        return {
+          id: lable.id,
+          bbox: lable.bbox
+        };
+      });
+      return {
+        data: sample.data.url || sample.data.coco_url || sample.data.flickr_url,
+        label: newLabels
+      };
+    },
+    metadata: async (meta: Coco.DatasetMeta): Promise<ObjectDetection.DatasetMeta> => {
+      const labelMap: Record<number, string> = {};
+      for (const labelName in meta.labelMap) {
+        labelMap[labelName] = meta.labelMap[labelName].name;
+      }
+      return {
+        type: meta.type,
+        size: meta.size,
+        labelMap
+      };
+    }
+  }, dataset);
+};
