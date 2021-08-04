563dea87ee9dcb5693fc0977c92ea83ba1ecf9f0
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-04 20:15:28 +0800

    load and save model

--- test/node/model/naive-bayes.ts
+++ test/node/model/naive-bayes.ts
@@ -23,9 +23,9 @@ const testLabels = tf.tensor1d([0, 0, 0, 5], 'int32');
 describe('Naive bayes', () => {
     it('predict train case', async () => {
         const mnb = new MultinomialNB();
-        await mnb.trainBatch(cases, labels);
-        await mnb.trainBatch(cases, labels);
-        // const mnb = new MultinomialNB(model);
+        // train twice
+        await mnb.train(cases, labels);
+        await mnb.train(cases, labels);
         const prediction = mnb.predict(cases);
         prediction.print();
         assert.deepEqual(prediction.dataSync(), labels.dataSync());
@@ -34,21 +34,29 @@ describe('Naive bayes', () => {
         const Xtest = tf.tensor2d([[2, 1, 0, 0, 0, 0],[3, 0, 0, 0, 1, 1],[1,0,0,0,1,1]]);
         const expection = tf.tensor1d([0]);
         const mnb = new MultinomialNB();
-        await mnb.trainBatch(cases, labels);
+        await mnb.train(cases, labels);
         const probs = mnb.predictProba(cases);
         const nCases = cases.shape[0];
         const class_prob_sum = tf.sum(probs, 1);
         const class_sum = new Array(nCases).fill(1);
         assert.deepEqual(class_prob_sum.arraySync(), class_sum);
-        //assert.deepEqual(prediction.dataSync(), labels.dataSync());
     });
     it('predict test case', async () => {
         const mnb = new MultinomialNB();
-        await mnb.trainBatch(cases, labels);
+        await mnb.train(cases, labels);
         const yPred = mnb.predict(testCases);
         yPred.print();
         assert.deepEqual(yPred.dataSync(), testLabels.dataSync());
     });
 
+    it('save and load model', async () => {
+        const mnb = new MultinomialNB();
+        await mnb.train(cases, labels);
+        const modelJson = mnb.toJson();
+        const mnb2 = new MultinomialNB();
+        mnb2.load(modelJson);
+        const yPred = mnb2.predict(testCases);
+        assert.deepEqual(yPred.dataSync(), testLabels.dataSync());
+    });
 
 })
