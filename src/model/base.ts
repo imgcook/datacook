95472dc23c1e4cefe2650a9bafa9979d7fa777ee
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-04 19:14:40 +0800

    multinomial naive bayes

---
+++ src/model/base.ts
@@ -0,0 +1,33 @@
+import { Tensor, tensor } from '@tensorflow/tfjs-core';
+import { checkArray } from '../utils/validation';
+
+export class BaseClassifier {
+    
+  public estimatorType: string = 'classifier';
+
+  public score( X: Tensor,  y: Tensor, sampleWeight?: Tensor ): Tensor {
+    const n_X = X.shape[0];
+    const n_y = y.shape[0];
+    if (n_X == 0 || n_y == 0) {
+        throw new Error('inputs should not have length of zero');
+    }
+    // TODO:
+    // Accuaracy score computation
+    const score = tensor([0]);
+    return score;
+  }
+
+  public validateData(X: any, y: any){
+    const X_tensor = checkArray(X, 'float32', 2);
+    const y_tensor = checkArray(y, 'int32', 1);
+    const n_X = X_tensor.shape[0];
+    const n_y = y_tensor.shape[0];
+    if (n_X != n_y) {
+      throw new RangeError(
+        "the size of the training set and the training labels must be the same."
+      )
+    }
+    return {'X': X_tensor, 'y': y_tensor}
+  }
+
+}
---
+++ test/node/utils/validation.ts
@@ -0,0 +1,26 @@
+import '@tensorflow/tfjs-backend-cpu';
+import { Tensor, cast, tensor, DataType } from '@tensorflow/tfjs-core';
+import { assert } from 'chai';
+import 'mocha';
+import { checkArray } from '../../../src/utils/validation';
+
+describe('Validation', function () {
+
+  describe('ArrayChecker', () => {
+    
+    it('convert array to int32 tensor', () => {
+        const array = [ 1, 2, 3 ]
+        const arrayTensor = checkArray(array, 'int32');
+        arrayTensor.print();
+        assert.equal(arrayTensor.dtype, 'int32');
+    });
+
+    it('convert two dimension array to int32 tensor', () => {
+      const array = [[ 1, 2, 3 ], [ 4, 5, 6 ]];
+      const arrayTensor = checkArray(array, 'int32');
+      arrayTensor.print();
+      assert.equal(arrayTensor.dtype, 'int32');
+    });
+    
+  })  
+})
