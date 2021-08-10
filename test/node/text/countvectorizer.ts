b2c3c81612b96137cd9d515b232463d630c957d5
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-11 00:52:56 +0800

    ammend vectorize

--- test/node/text/countvectorizer.ts
+++ test/node/text/countvectorizer.ts
@@ -23,4 +23,25 @@ describe('CountVectorizer', function(){
     assert.deepEqual(vectorizer.wordOrder, vocab);
     assert.deepEqual(toVector, expectedVector);
   });
+  it('should convert words array to their proper countvectorizer', function(){
+    const vectorizer = new CountVectorizer([['The'],['quick'], ['brown'], ['fox'], ['jumped'], ['over'], ['the'], ['lazy'], ['dog']]);
+    const toVector = vectorizer.transform(["the brown brown","the the brown"]);
+    const vocab = {
+                    brown: 0,
+                    dog: 1,
+                    fox: 2,
+                    jumped: 3,
+                    lazy: 4,
+                    over: 5,
+                    quick: 6,
+                    the: 7
+                  };
+    const expectedVector = [
+                            [2, 0, 0, 0,0, 0, 0, 1],
+                            [1, 0, 0, 0,0, 0, 0, 2]
+                           ];
+    console.log(vectorizer.wordOrder);
+    assert.deepEqual(vectorizer.wordOrder, vocab);
+    assert.deepEqual(toVector, expectedVector);
+  });
 });
