66b5c5a1aeea987ef816b221247d1e66ae5cc818
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-13 16:59:59 +0800

    vecorize

--- test/node/text/countvectorizer.ts
+++ test/node/text/countvectorizer.ts
@@ -3,8 +3,8 @@ import CountVectorizer from "../../../src/text/countvectorizer";
 
 describe('CountVectorizer', function(){
   it('should convert words to their proper countvectorizer', function(){
-    const vectorizer = new CountVectorizer(["The quick brown fox jumped over the lazy dog."]);
-    const toVector = vectorizer.transform(["the brown brown","the the brown"]);
+    const vectorizer = new CountVectorizer(["The quick brown fox jumped over the lazy big fat dog."], ['big', 'fat']);
+    const toVector = vectorizer.transform(["the brown brown","the the brown hat car"]);
     const vocab = {
                     brown: 0,
                     dog: 1,
@@ -24,7 +24,7 @@ describe('CountVectorizer', function(){
     assert.deepEqual(toVector, expectedVector);
   });
   it('should convert words array to their proper countvectorizer', function(){
-    const vectorizer = new CountVectorizer([['The'],['quick'], ['brown'], ['fox'], ['jumped'], ['over'], ['the'], ['lazy'], ['dog']]);
+    const vectorizer = new CountVectorizer([['The'],['quick'], ['brown'], ['fox'], ['jumped'], ['over'], ['the'], ['lazy'], ['dog']], ['the']);
     const toVector = vectorizer.transform(["the brown brown","the the brown"]);
     const vocab = {
                     brown: 0,
@@ -33,12 +33,11 @@ describe('CountVectorizer', function(){
                     jumped: 3,
                     lazy: 4,
                     over: 5,
-                    quick: 6,
-                    the: 7
+                    quick: 6
                   };
     const expectedVector = [
-                            [2, 0, 0, 0,0, 0, 0, 1],
-                            [1, 0, 0, 0,0, 0, 0, 2]
+                            [2, 0, 0, 0,0, 0, 0],
+                            [1, 0, 0, 0,0, 0, 0]
                            ];
     console.log(vectorizer.wordOrder);
     assert.deepEqual(vectorizer.wordOrder, vocab);
