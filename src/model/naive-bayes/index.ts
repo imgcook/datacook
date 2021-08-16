c750d6378e3e0358fa197d3e8f61bc70a746b51d
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-16 15:54:42 +0800

    support string label

--- src/model/naive-bayes/index.ts
+++ src/model/naive-bayes/index.ts
@@ -82,7 +82,6 @@ export class MultinomialNB extends BaseClassifier {
    */
   public async train(xData: Array<any> | Tensor, yData: Array<any> | Tensor): Promise<MultinomialNB> {
     const { x, y } = this.validateData(xData, yData);
-
     const firstCall = this.firstCall();
     let yOneHot;
 
