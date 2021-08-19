1f7b959bdf97af9fba0d18ef7fd7984b43aee098
FeelyChau <feely@outlook.com>
2021-08-19 11:41:40 +0800

    dataset: add annotation for dataset helper

--- src/dataset/format/pascal-voc.ts
+++ src/dataset/format/pascal-voc.ts
@@ -5,6 +5,11 @@ function isAnnotationArray(arg: PascalVoc.PascalVocObject[] | PascalVoc.PascalVo
   return Array.isArray(arg);
 }
 
+/**
+ * Make dataset from pascal-voc.
+ * @param annotations Pascal-voc annotations.
+ * @returns Dataset for pascal-voc.
+ */
 export function makeDatasetFromPascalVoc(annotations: Array<PascalVoc.Annotation>): Dataset<PascalVoc.Sample> {
   const samples: PascalVoc.Sample[] = annotations.map((annotation: PascalVoc.Annotation) => {
     return {
@@ -15,6 +20,11 @@ export function makeDatasetFromPascalVoc(annotations: Array<PascalVoc.Annotation
   return new ArrayDatasetImpl<PascalVoc.Sample>(samples);
 }
 
+/**
+ * Extract categories from pascal-voc.
+ * @param annotations Pascal-voc annotations.
+ * @returns Array of string which includes categories.
+ */
 export function extractCategoriesFromPascalVoc(annotations: Array<PascalVoc.Annotation>): Array<string> {
   const labelSet = new Set<string>();
   annotations.forEach((annotation: PascalVoc.Annotation) => {
--- src/dataset/text-classification.ts
+++ src/dataset/text-classification.ts
@@ -1,6 +1,11 @@
 import { Dataset, TextClassification } from './types';
 import { ArrayDatasetImpl } from './';
 
+/**
+ * Make dataset for text classification.
+ * @param textList The list of category and sample text.
+ * @returns Dataset for text classification.
+ */
 export function makeTextClassificationDatasetFromList(textList: TextClassification.TextList): Dataset<TextClassification.Sample> {
   const data = textList.map((textDesc) => {
     return {
