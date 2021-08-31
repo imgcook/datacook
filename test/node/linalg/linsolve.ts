96981f480bea78a8fbdc343ddd2add5df3efba8c
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-08-31 15:20:49 +0800

    merge test

--- test/node/linalg/linsolve.ts
+++ test/node/linalg/linsolve.ts
@@ -1,9 +1,5 @@
 import '@tensorflow/tfjs-backend-cpu';
-<<<<<<< HEAD
 import { tensor, matMul, reshape, sub, squeeze } from '@tensorflow/tfjs-core';
-=======
-import { tensor, matMul, reshape, sub } from '@tensorflow/tfjs-core';
->>>>>>> 3656f635f8e7ba2b74385608a932249c54c1bed4
 import { tensorEqual } from '../../../src/linalg/utils';
 import { linSolveQR } from '../../../src/linalg/linsolve';
 import { assert } from 'chai';
@@ -16,16 +12,9 @@ const matrix = tensor([
 const v = tensor([0, 8, 10]);
 
 describe('linSolver', () => {
-<<<<<<< HEAD
   it('solve matrix', () => {
     const solve = linSolveQR(matrix, v);
     const predv = squeeze(matMul(matrix, reshape(solve, [-1, 1])));
-=======
-  
-  it('solve matrix', () => {
-    const solve = linSolveQR(matrix, v);
-    const predv = matMul(matrix, reshape(solve, [-1, 1]));
->>>>>>> 3656f635f8e7ba2b74385608a932249c54c1bed4
     const equal = tensorEqual(v, predv, 1e-3);
     assert.isTrue(equal);
   })
