5691782343ac46fae672ad2fa64cc9536908d71f
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-09-06 13:41:05 +0800

    merge

--- src/model/base.ts
+++ src/model/base.ts
@@ -1,4 +1,4 @@
-import { Tensor, tensor, RecursiveArray, unique, cast, oneHot, argMax, reshape, stack, slice, greater } from '@tensorflow/tfjs-core';
+import { Tensor, tensor, RecursiveArray, unique, cast, oneHot } from '@tensorflow/tfjs-core';
 import { checkArray } from '../utils/validation';
 
 export type ClassMap = {
@@ -39,18 +39,19 @@ export class BaseClassifier {
     const yData = y.dataSync();
     const nClass = this.classes.shape[0];
     const yInd = yData.map((d: number|string) => this.classMap[d]);
-    if (nClass > 2) {
-      const yOneHot = oneHot(cast(tensor(yInd), 'int32'), nClass);
-      return yOneHot;
-    } else {
-      return cast(tensor(yInd), 'int32');
-    }
+    const yOneHot = oneHot(cast(tensor(yInd), 'int32'), nClass);
+    return yOneHot;
   }
 
   public initClasses(y: Tensor): void {
     const { values } = unique(y);
     this.classes = values;
     this.updateClassMap();
+  }
+
+  public isBinaryClassification(): boolean {
+    const nClass = this.classes.shape[0];
+    return nClass == 2;
   }
 
   public validateData(x: Tensor | RecursiveArray<number>, y: Tensor | RecursiveArray<number>, xDimension = 2, yDimension = 1): { x: Tensor, y: Tensor } {
@@ -65,21 +66,8 @@ export class BaseClassifier {
     }
     return { 'x': xTensor, 'y': yTensor };
   }
-
-  public isBinaryClassification(): boolean {
-    const nClass = this.classes.shape[0];
-    return nClass == 2;
-  }
-
-  public getPredClass(score: Tensor): Tensor {
-    const axisH = 1;
-    const classInd = (this.isBinaryClassification) ? greater(score, 0.5).dataSync() : argMax(score, axisH).dataSync();
-    const classTensors: Tensor[] = [];
-    classInd.forEach((i: number) => { return classTensors.push(slice(this.classes, [ i ], [ 1 ])); });
-    const classVal = reshape(stack(classTensors), [ -1 ]);
-    return classVal;
-  }
 }
+
 
 export class BaseEstimater {
   // TODO(sugarspectre): Add evaluation functions
