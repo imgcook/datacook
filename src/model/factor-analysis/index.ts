260abbaf47b6bf1ad9b0ad21c9408f1dc828133b
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-09-13 15:46:56 +0800

    fa

--- src/model/factor-analysis/index.ts
+++ src/model/factor-analysis/index.ts
@@ -1,3 +1,14 @@
+import { Tensor, mean, matMul, sub, transpose, RecursiveArray, ones, sqrt, add, divNoNan } from "@tensorflow/tfjs-core";
+import { setFlagsFromString } from "node:v8";
+import { svd } from "../../linalg";
+import { checkArray } from "../../utils/validation";
+
+export type FactorAnalysisParams = {
+  nComponent: number,
+  tol?: number,
+  maxIterTimes?: number
+}
+
 /**
  * Factor Analysis.
  *
@@ -9,25 +20,31 @@
  * The implementation of factor analysis uses an EM-based method to find the best
  * solution for factor loading.
  */
- import { svd } from "../../linalg";
+export class FactorAnalysis {
+  public nComponents: number;
+  public tol: number;
+  public maxIterTimes: number;
 
- export type FactorAnalysisParams = {
-   nComponent: number,
-   tol?: number,
-   maxIterTimes?: number
- }
- 
- export class FactorAnalysis {
-   public nComponents: number;
-   public tol: number;
- 
-   constructor(params: FactorAnalysisParams) {
- 
-   }
- 
-   public fit(xData: ) {
- 
-   }
- }
- 
- 
+  constructor(params: FactorAnalysisParams) {
+    this.nComponents = params.nComponent ? params.nComponent : -1;
+    this.tol = params.tol ? params.tol : 1e-2;
+    this.maxIterTimes = params.maxIterTimes ? params.maxIterTimes : 10000;
+  }
+
+  public fit(xData: Tensor | RecursiveArray<number>): void {
+    const xTensor = checkArray(xData, 'float32', 2);
+    const [ nSamples, nFeatures ] = xTensor.shape;
+    const nComponent = (!this.nComponents || this.nComponents === -1 || this.nComponents > nFeatures) ? nFeatures : this.nComponents;
+    const xMeans = mean(xTensor, 0);
+    const nSqrt = Math.sqrt(nSamples);
+    const xCentered = sub(xTensor, xMeans);
+    const s = matMul(transpose(xCentered), xCentered);
+    const small = 1e-12;
+    let psi = ones([ nFeatures ]);
+    /*for (let i = 0; i < this.maxIterTimes; i++) {
+      const sqrtPsi = add(sqrt(psi), small);
+      const [ u, v, m ] = svd(divNoNan(xCentered, ))
+    }
+    */
+  }
+}
