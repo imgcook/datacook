ab55b7cc8ea05d9d1c7c3aafe9ba39f9bf5c966a
数斯 <wangxiaojian.wxj@alibaba-inc.com>
2021-09-14 13:20:03 +0800

    fa

--- src/model/factor-analysis/index.ts
+++ src/model/factor-analysis/index.ts
@@ -1,5 +1,5 @@
-import { Tensor, mean, matMul, sub, transpose, RecursiveArray, ones, sqrt, add, divNoNan, mul } from "@tensorflow/tfjs-core";
-import { svd } from "../../linalg";
+import { Tensor, mean, matMul, sub, transpose, RecursiveArray, ones, sqrt, add, divNoNan, mul, gather, slice, linalg, diag, eye, log, sum } from "@tensorflow/tfjs-core";
+import { svd, tensorEqual } from "../../linalg";
 import { checkArray } from "../../utils/validation";
 
 export type FactorAnalysisParams = {
@@ -23,6 +23,7 @@ export class FactorAnalysis {
   public nComponents: number;
   public tol: number;
   public maxIterTimes: number;
+  public factorLoadings: Tensor;
 
   constructor(params: FactorAnalysisParams) {
     this.nComponents = params.nComponent ? params.nComponent : -1;
@@ -30,20 +31,36 @@ export class FactorAnalysis {
     this.maxIterTimes = params.maxIterTimes ? params.maxIterTimes : 10000;
   }
 
-  public fit(xData: Tensor | RecursiveArray<number>): void {
+  public async fit(xData: Tensor | RecursiveArray<number>): Promise<FactorAnalysis> {
     const xTensor = checkArray(xData, 'float32', 2);
     const [ nSamples, nFeatures ] = xTensor.shape;
     const nComponent = (!this.nComponents || this.nComponents === -1 || this.nComponents > nFeatures) ? nFeatures : this.nComponents;
     const xMeans = mean(xTensor, 0);
     const nSqrt = Math.sqrt(nSamples);
     const xCentered = sub(xTensor, xMeans);
-    const s = matMul(transpose(xCentered), xCentered);
+    const sigmaDiag = linalg.bandPart(matMul(transpose(xCentered), xCentered), 0, 0);
     const small = 1e-12;
+    let oldll: Tensor;
     let psi = ones([ nFeatures ]);
     for (let i = 0; i < this.maxIterTimes; i++) {
       const sqrtPsi = add(sqrt(psi), small);
-      const [ u, v, m ] = svd(divNoNan(xCentered, mul(sqrtPsi, nSqrt)));
-      
+      const xd = divNoNan(xCentered, mul(sqrtPsi, nSqrt));
+      const [ u, m, w ] = await svd(xd);
+      const firstNInd = [ ...Array(nComponent).keys() ];
+      const uh = gather(u, firstNInd, 1);
+      const mh = gather(mul(m, m), firstNInd);
+      const f = matMul(matMul(sqrtPsi, uh), sqrt(sub(diag(mh), eye(nComponent))));
+      // update pasi
+      psi = sub(sigmaDiag, diag(matMul(transpose(f), f)));
+      const llDelta = add(sum(log(psi)), sum(log(mh)));
+      if (oldll && tensorEqual(llDelta, oldll, this.tol)) {
+        this.factorLoadings = f;
+        break;
+      }
+      if (i == this.maxIterTimes - 1) {
+        this.factorLoadings = f;
+      }
     }
+    return this;
   }
 }
