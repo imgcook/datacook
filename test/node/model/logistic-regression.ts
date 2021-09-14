4c48c476a90299fe3843b42caa8bcc2e185aa87d
Jan Wang <wangxjbuaa@foxmail.com>
2021-09-14 12:28:10 +0800

    model: Multi-classification for LR and test cases (#68)

--- test/node/model/logistic-regression.ts
+++ test/node/model/logistic-regression.ts
@@ -1,52 +1,95 @@
 import { LogisticRegression } from '../../../src/model/linear-model/logistic-regression';
+import '@tensorflow/tfjs-backend-cpu';
 import { LogisticRegressionPredictor } from '../../../src/model/linear-model/logistic-regression-predictor';
 import * as tf from '@tensorflow/tfjs-core';
 import { assert } from 'chai';
 import 'mocha';
 import { Tensor } from '@tensorflow/tfjs-core';
 import { accuracyScore } from '../../../src/metrics/classifier';
+import { tensorEqual } from '../../../src/linalg/utils';
 
-const cases = tf.mul(tf.randomNormal([10000, 5]),[ 10, 10, 10, 20, 10 ]);
-const weight = tf.tensor([ 2, 3, 1, 4, 6 ])
-const eta = tf.add(tf.sum(tf.mul(cases, weight), 1), -20);
-const y = tf.greater(tf.sigmoid(eta), 0.5);
+const cases = tf.mul(tf.randomNormal([ 10000, 5 ]), [ 100, 100, 200, 100, 10 ]);
+const weight = tf.tensor([ 2, 3, 1, -4, 6 ])
+const eta = tf.add(tf.sum(tf.mul(cases, weight), 1), -20).arraySync() as number[];
+const y = tf.greater(eta, 0);
+const yMult = eta.map((e: number): number => e < -200 ? 1 : e < 0 ? 2 : e < 200 ? 3 : 4);
 
 describe('Logistic ', () => {
 
   it('predict class probabilities', async () => {
-  const lr = new LogisticRegression({fitIntercept: true});
-    await lr.fit(cases, y); 
+    const lr = new LogisticRegression({optimizerType: 'sgd', optimizerProps: {learningRate: 0.1}});
+    await lr.fit(cases, y);
     const probs = await lr.predictProba(cases);
-    const trueCount = tf.sum(tf.lessEqual(probs, 1)).dataSync()[0];
-    assert.deepEqual(trueCount, 10000);
+    assert.equal(tf.sum(tf.lessEqual(probs, 1)).dataSync() as any, 10000);
+  });
+
+  it('predict class probabilities (multi-classification)', async () => {
+  const lr = new LogisticRegression({optimizerType: 'sgd', optimizerProps: {learningRate: 0.1}});
+    await lr.fit(cases, yMult);
+    const probs = await lr.predictProba(cases);
+    assert.isTrue(tensorEqual(tf.sum(probs, 1), tf.ones([ 10000 ]), 1e-5));
   });
 
   it('train simple dataset', async () => {
-    const lr = new LogisticRegression({fitIntercept: true});
-    await lr.fit(cases, y); 
+    const lr = new LogisticRegression();
+    await lr.fit(cases, y);
     const predY = await lr.predict(cases);
     if (predY instanceof Tensor){
       const acc = accuracyScore(y, predY);
+      console.log('accuracy: ', acc);
       assert.isTrue(acc >= 0.95);
     }
   });
 
+  it('train simple dataset (multi-classification)', async () => {
+    const lr = new LogisticRegression();
+    await lr.fit(cases, yMult);
+    const predY = await lr.predict(cases);
+    if (predY instanceof Tensor){
+      const acc = accuracyScore(yMult, predY);
+      console.log('accuracy: ', acc);
+      assert.isTrue(acc >= 0.75);
+    }
+  });
+
   it('train simple dataset on batch', async () => {
-    const lr = new LogisticRegression({optimizerType: 'adam', optimizerProps: {learningRate: 0.01}});	
+    const lr = new LogisticRegression();
+    await lr.initClasses([ 0, 1 ], 'binary-only');
+    const batchSize = 32;
     for (let i = 0; i < 800; i++) {
-      const j = Math.floor(i%(100));
-      const batchX = tf.slice(cases, [j * 100, 0], [100 ,5]);
-      const batchY = tf.slice(y, [j * 100], [100]);
+      const j = Math.floor(i%(batchSize));
+      const batchX = tf.slice(cases, [j * batchSize, 0], [batchSize ,5]);
+      const batchY = tf.slice(y, [j * batchSize], [batchSize]);
       await lr.trainOnBatch(batchX, batchY)
     }
     const predY = await lr.predict(cases);
     if (predY instanceof Tensor){
       const acc = accuracyScore(y, predY);
+      console.log('accuracy: ', acc);
       assert.isTrue(acc >= 0.95);
     }
   });
+
+  it('train simple dataset on batch (multi-classification)', async () => {
+    const lr = new LogisticRegression();
+    // class initialization is important for batch training
+    await lr.initClasses([ 1, 2, 3, 4 ], 'binary-only');
+    const batchSize = 32;
+    for (let i = 0; i < 800; i++) {
+      const j = Math.floor(i%(batchSize));
+      const batchX = tf.slice(cases, [j * batchSize, 0], [batchSize ,5]);
+      const batchY = tf.slice(yMult, [j * batchSize], [batchSize]);
+      await lr.trainOnBatch(batchX, batchY)
+    }
+    const predY = await lr.predict(cases);
+    if (predY instanceof Tensor){
+      const acc = accuracyScore(yMult, predY);
+      console.log('accuracy: ', acc);
+      assert.isTrue(acc >= 0.75);
+    }
+  });
   it('save and load model', async () => {
-    const lr = new LogisticRegression({fitIntercept: true});
+    const lr = new LogisticRegression();
     await lr.fit(cases, y); 
     const modelJson = await lr.toJson();
     const lr2 = new LogisticRegression();
@@ -54,11 +97,25 @@ describe('Logistic ', () => {
     const predY = await lr2.predict(cases);
     if (predY instanceof Tensor){
       const acc = accuracyScore(y, predY);
+      console.log('accuracy: ', acc);
       assert.isTrue(acc >= 0.95);
     }
   });
+  it('save and load model (multi-classification)', async () => {
+    const lr = new LogisticRegression();
+    await lr.fit(cases, yMult); 
+    const modelJson = await lr.toJson();
+    const lr2 = new LogisticRegression();
+    await lr2.fromJson(modelJson);
+    const predY = await lr2.predict(cases);
+    if (predY instanceof Tensor){
+      const acc = accuracyScore(yMult, predY);
+      console.log('accuracy: ', acc);
+      assert.isTrue(acc >= 0.75);
+    }
+  });
   it('save and load model as predictor', async () => {
-    const lr = new LogisticRegression({fitIntercept: true});
+    const lr = new LogisticRegression();
     await lr.fit(cases, y); 
     const modelJson = await lr.toJson();
     const lr2 = new LogisticRegressionPredictor();
@@ -66,7 +123,21 @@ describe('Logistic ', () => {
     const predY = await lr2.predict(cases);
     if (predY instanceof Tensor){
       const acc = accuracyScore(y, predY);
+      console.log('accuracy: ', acc);
       assert.isTrue(acc >= 0.95);
+    }
+  });
+  it('save and load model as predictor (multi-classification)', async () => {
+    const lr = new LogisticRegression();
+    await lr.fit(cases, yMult); 
+    const modelJson = await lr.toJson();
+    const lr2 = new LogisticRegressionPredictor();
+    await lr2.fromJson(modelJson);
+    const predY = await lr2.predict(cases);
+    if (predY instanceof Tensor){
+      const acc = accuracyScore(yMult, predY);
+      console.log('accuracy: ', acc);
+      assert.isTrue(acc >= 0.75);
     }
   });
 });
