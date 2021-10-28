5eac77a6084827e252abe9a2b3fabb2c7a3f1e48
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2021-10-28 20:17:29 +0800

    kmeans

--- src/model/clustering/kmeans/index.ts
+++ src/model/clustering/kmeans/index.ts
@@ -1,38 +1,139 @@
 import { BaseClustering, FeatureInputType } from "../../base";
 import { tensorEqual } from "../../../linalg";
 import { Tensor, sub, norm, gather, stack, min, sum, slice, add, tensor, mul,
-  transpose, mean, booleanMaskAsync, equal, argMin, divNoNan, squeeze, zeros, reshape } from "@tensorflow/tfjs-core";
+  transpose, mean, booleanMaskAsync, equal, argMin, divNoNan, squeeze, zeros, reshape, RecursiveArray, square, neg } from "@tensorflow/tfjs-core";
+import { shuffle } from "../../../generic";
+import { checkArray } from "../../../utils/validation";
 
-export type KMeansInitType = 'kmeans++' | 'random';
-const defaultMaxIterTimes = 10000;
+export type KMeansInitType = 'kmeans++' | 'random' | Tensor | RecursiveArray<number>;
+const defaultMaxIterTimes = 1000;
 const defaultTol = 1e-5;
+const defaultNInit = 10;
+const defaultInit = 'kmeans++';
+
 export type KMeansParams = {
   nClusters: number,
   tol?: number,
+  nInit?: number,
   maxIterTimes?: number,
-  randomState?: number
+  init?: KMeansInitType,
 }
 
 export class KMeans extends BaseClustering {
   public nClusters: number;
   public init: KMeansInitType;
+  public nInit: number;
   public maxIterTimes: number;
   public tol: number;
   public randomState: number;
   public centroids: Tensor;
-  public clusIndex: Tensor;
-  private clusCount: Tensor;
+  private clusWeightedSum: Tensor;
   private firstTrainOnBatch: boolean;
 
+
+  /**
+   * Initialize KMeans model.
+   * @param params parameter object used to initialize KMeans model.
+   *
+   * Options in params
+   * ------------
+   *
+   * `nClusters` : number, default=8\
+   *    The number of clusters to form as well as the number of centroids
+   *    to generate.
+   *
+   * `init`: {'kmeans++', 'random'} or an array of shape (nClusters, nFeatures),
+   *    default = 'kmeans++'
+   *
+   *    - 'kmeans++': select initial cluster centroids using kmeans++
+   *    - 'random': randomly select initial centroids
+   *    - array of shape (nClusters, nFeatures): use user defined centroids.
+   *
+   * `nInit`: number, default=10.\
+   *    Number of time the algorithm will be run with different initialization
+   *    centroid seeds. The final results will be the best output of nInit
+   *    consecutive runs in terms of inertia.
+   *
+   *  `maxIterTimes` : number, default=1000\
+   *    Maximum number of iterations of the k-means algorithm for a single run.
+   *
+   *  `tol` : number, default=1e-5\
+   *    Relative tolerance with regards to Frobenius norm of the difference
+   *    in the cluster centers of two consecutive iterations to declare
+   *    convergence.
+   */
   constructor(params: KMeansParams) {
     super();
     if (!params.nClusters) {
       throw new TypeError('nClusters is not specified');
     }
+    this.nInit = params.nInit ? params.nInit : defaultNInit;
     this.nClusters = params.nClusters;
     this.maxIterTimes = params.maxIterTimes ? params.maxIterTimes : defaultMaxIterTimes;
     this.tol = params.tol ? params.tol : defaultTol;
     this.firstTrainOnBatch = true;
+    this.init = params.init ? params.init : defaultInit;
+  }
+
+  /**
+   * Get a set of centroids according to input tensor and type of initialize
+   * @param xTensor
+   * @returns
+   */
+  public getInitCentroids(xTensor: Tensor): Tensor {
+    if (this.init === 'kmeans++') {
+      return this.kmeansPlusPlus(xTensor, this.nClusters);
+    }
+    if (this.init === 'random') {
+      const shuffleIndices = Array.from(Array(xTensor.shape[0]).keys());
+      shuffle(Array.from(Array(xTensor.shape[0]).keys()));
+      return gather(xTensor, shuffleIndices.slice(this.nClusters));
+    }
+    if (this.init instanceof Tensor || this.init instanceof Array) {
+      const initTensor = checkArray(this.init, 'float32', 2);
+      this.validataCentroidsShape(xTensor, initTensor);
+      return initTensor;
+    }
+    return this.kmeansPlusPlus(xTensor, this.nClusters);
+  }
+
+  /**
+   * Initialize centroids
+   * @param xTensor
+   * @returns centroids seledted from nInit iterations
+   */
+  public async initCentroids(xTensor: Tensor): Promise<Tensor> {
+    // If initialized centroids are defined
+    if (this.init instanceof Tensor || this.init instanceof Array) {
+      return this.getInitCentroids(xTensor);
+    }
+    // Initialize for nInit times to find the best centroids according to interia criteria.
+    let minInertia = Number.MAX_SAFE_INTEGER;
+    let selectedCentroids: Tensor;
+    for (let i = 0; i < this.nInit; i++) {
+      const centroids = this.getInitCentroids(xTensor);
+      const { newCentroids } = await this.update(xTensor, centroids);
+      const inertia = this.inertiaDense(xTensor, newCentroids);
+      if (inertia < minInertia) {
+        minInertia = inertia;
+        selectedCentroids = newCentroids;
+      }
+    }
+    return selectedCentroids;
+  }
+
+  /**
+   * Compute inertia for dense input data and given centroids.
+   * Sum of squared distance between each sample and its assigned center.
+   * @param xTensor
+   * @param centroids
+   * @returns computed inertia
+   */
+  public inertiaDense(xTensor: Tensor, centroids: Tensor): number {
+    const axisH = 1;
+    const dists = this.getClusDist(xTensor, centroids);
+    const minDists = min(dists, axisH);
+    return sum(square(minDists)).dataSync()[0];
   }
 
   /**
@@ -66,6 +167,30 @@ export class KMeans extends BaseClustering {
     return stack(centroids);
   }
 
+  /**
+   * Check if centers is compatible with X and nClusters
+   * @param xTensor
+   * @param centroids
+   */
+  public validataCentroidsShape(xTensor: Tensor, centroids: Tensor): void {
+    if (centroids.shape[1] !== this.nClusters) {
+      throw new TypeError(
+        `The shape of the initial centers ${centroids.shape} does not match the number of clusters ${this.nClusters}.`
+      );
+    }
+    if (centroids.shape[1] !== xTensor.shape[1]) {
+      throw new TypeError(
+        `The shape of the initial centers ${centroids.shape[1]} does not match the number of features of the data ${xTensor.shape[1]}.`
+      );
+    }
+  }
+
+  /**
+   * Get distance from data samples to their assigned cluster centers.
+   * @param xTensor
+   * @param centroids
+   * @returns
+   */
   public getClusDist(xTensor: Tensor, centroids: Tensor): Tensor {
     const axisH = 1;
     const newDistsData: Tensor[] = [];
@@ -76,23 +201,27 @@ export class KMeans extends BaseClustering {
     return transpose(stack(newDistsData));
   }
 
-  public getClusIndex(xTensor: Tensor): Tensor {
-    const newDists = this.getClusDist(xTensor, this.centroids);
+
+  /**
+   * Get the cluster index of given samples
+   * @param xTensor
+   * @returns
+   */
+  public getClusIndex(xTensor: Tensor, centroids: Tensor): Tensor {
+    const newDists = this.getClusDist(xTensor, centroids);
     return argMin(newDists, 1);
   }
 
-  // public async updateChunk(xTensor: Tensor, weights: Tensor): Promise<{ newCentroids: Tensor, newClusIndex: Tensor, weights: number }> {
-  //   const axisH = 1;
-  //   //const dists = this.getClusDist(xTensor, this.centroids);
-  //   const clusIndex = this.getClusIndex(xTensor);
-  //   const { newCentroids, newClusIndex } = this.update(xTensor);
-  //   //const = this.getClusIndex(xTensor);
-  // }
-
-  public async update(xTensor: Tensor): Promise<{ newCentroids: Tensor, newClusIndex: Tensor }> {
+  /**
+   * Update centroids in one step.
+   * @param xTensor input tensor
+   * @param centroids centroids
+   * @returns `{ newCentroids, newClusIndex }`
+   */
+  public async update(xTensor: Tensor, centroids: Tensor): Promise<{ newCentroids: Tensor, newClusIndex: Tensor }> {
     const axisV = 0;
     const newCentroidsData: Tensor[] = [];
-    const newClusIndex = this.getClusIndex(xTensor);
+    const newClusIndex = this.getClusIndex(xTensor, centroids);
     for (let i = 0; i < this.nClusters; i++) {
       const mask = equal(newClusIndex, i);
       const centroidI = await booleanMaskAsync(xTensor, mask);
@@ -109,27 +238,31 @@ export class KMeans extends BaseClustering {
    */
   public async fit(xData: FeatureInputType): Promise<void> {
     const xTensor = this.validateData(xData, true);
-    this.centroids = this.kmeansPlusPlus(xTensor, this.nClusters);
+    this.centroids = await this.initCentroids(xTensor);
     for (let i = 0; i < this.maxIterTimes; i++) {
-      const { newCentroids, newClusIndex } = await this.update(xTensor);
+      const { newCentroids } = await this.update(xTensor, this.centroids);
       if (tensorEqual(newCentroids, this.centroids, this.tol)) {
         return;
       }
       this.centroids = newCentroids;
-      this.clusIndex = newClusIndex;
     }
   }
 
+  /**
+   * Train kmeans model by batch. Here we apply mini-batch kmeans algorithm to
+   * update centroids in each iteration.
+   * @param xData
+   */
   public async trainOnBatch(xData: FeatureInputType): Promise<void> {
     const xTensor = this.validateData(xData, this.firstTrainOnBatch);
     if (this.firstTrainOnBatch) {
-      this.centroids = this.kmeansPlusPlus(xTensor, this.nClusters);
+      this.centroids = await this.initCentroids(xTensor);
       this.firstTrainOnBatch = false;
-      this.clusCount = zeros([ this.nClusters ]);
+      this.clusWeightedSum = zeros([ this.nClusters ]);
     }
     const axisV = 0;
     const batchCentroidsData: Tensor[] = [];
-    const newClusIndex = this.getClusIndex(xTensor);
+    const newClusIndex = this.getClusIndex(xTensor, this.centroids);
     const clusCount: number[] = [];
     for (let i = 0; i < this.nClusters; i++) {
       const mask = equal(newClusIndex, i);
@@ -137,17 +270,76 @@ export class KMeans extends BaseClustering {
       const centroidI = await booleanMaskAsync(xTensor, mask);
       batchCentroidsData.push(mean(centroidI, axisV));
     }
-    // update cluster count
-    this.clusCount = add(this.clusCount, tensor(clusCount));
     const batchCentroids = stack(batchCentroidsData);
-    //const eta = reshape(divNoNan(1, this.clusCount), [ -1, 1 ]);
-    const eta = 0.5;
-    // c = (1 - eta) * c + eta * x
-    this.centroids = add(mul(this.centroids, sub(1, eta)), mul(batchCentroids, eta));
+    const newClusWeightedSum = add(this.clusWeightedSum, tensor(clusCount));
+    // update centroids
+    this.centroids = add(mul(this.centroids, reshape(this.clusWeightedSum, [ -1, 1 ])), mul(batchCentroids, reshape(clusCount, [ -1, 1 ])));
+    this.centroids = divNoNan(this.centroids, reshape(newClusWeightedSum, [ -1, 1 ]));
+    // update cluster count
+    this.clusWeightedSum = newClusWeightedSum;
   }
 
+  /**
+   * Predict sample clusters for given input.
+   * @param xData
+   * @returns predicted cluster index
+   */
   public async predict(xData: FeatureInputType): Promise<Tensor> {
     const xTensor = this.validateData(xData, false);
-    return this.getClusIndex(xTensor);
+    return this.getClusIndex(xTensor, this.centroids);
   }
+
+  /**
+   * Get scores for input xData on the kmeans model.\
+   * score = -inertia, larger score usually represent better fit.
+   * @param xData
+   * @returns
+   */
+  public async score(xData: FeatureInputType): Promise<number> {
+    const xTensor = this.validateData(xData, false);
+    const interia = this.inertiaDense(xTensor, this.centroids);
+    return neg(interia).dataSync()[0];
+  }
+
+  /**
+   * Load model paramters from json string object
+   * @param modelJson model json saved as string object
+   * @returns model itself
+   */
+  public async fromJson(modelJson: string): Promise<KMeans> {
+    const params = JSON.parse(modelJson);
+    if (params.name !== 'KMeans') {
+      throw new TypeError(`${params.name} is not KMeans`);
+    }
+    if (!params.nClusters) {
+      throw new TypeError('nClusters is not specified');
+    }
+    this.nInit = params.nInit ? params.nInit : defaultNInit;
+    this.nClusters = params.nClusters;
+    this.maxIterTimes = params.maxIterTimes ? params.maxIterTimes : defaultMaxIterTimes;
+    this.tol = params.tol ? params.tol : defaultTol;
+    this.firstTrainOnBatch = true;
+    this.init = params.init ? params.init : defaultInit;
+    this.centroids = params.centroids ? tensor(params.centroids) : null;
+    return this;
+  }
+
+  /**
+   * Dump model parameters to json string.
+   * @returns Stringfied model parameters
+   */
+  public async toJson(): Promise<string> {
+    const modelParams = {
+      name: 'KMeans',
+      nInit: this.nInit,
+      nClusters: this.nClusters,
+      maxIterTimes: this.maxIterTimes,
+      tol: this.tol,
+      firstTrainOnBatch: this.firstTrainOnBatch,
+      init: this.init,
+      centroids: await this.centroids.array()
+    };
+    return JSON.stringify(modelParams);
+  }
+
 }
--- test/node/model/kmeans.ts
+++ test/node/model/kmeans.ts
@@ -1,24 +1,39 @@
 import { KMeans } from '../../../src/model';
 import '@tensorflow/tfjs-backend-cpu';
 import * as tf from '@tensorflow/tfjs-core';
-import { assert } from 'chai';
-import { tensorEqual } from '../../../src/linalg/utils';
 import 'mocha';
+import { assert } from 'chai';
 import { Tensor } from '@tensorflow/tfjs-core';
 
-const clust1 = tf.add(tf.mul(tf.randomNormal([ 100, 2 ]), tf.tensor([ 3, 4 ])), tf.tensor([ 5, 5 ]));
-const clust2 = tf.add(tf.mul(tf.randomNormal([ 100, 2 ]), tf.tensor([ 2, 3 ])), tf.tensor([ 10, 0 ]));
-const clust3 = tf.add(tf.mul(tf.randomNormal([ 100, 2 ]), tf.tensor([ 4, 3 ])), tf.tensor([ -10, 0 ]));
+const clust1 = tf.add(tf.mul(tf.randomNormal([ 100, 2 ]), tf.tensor([ 2, 2 ])), tf.tensor([ 5, 5 ]));
+const clust2 = tf.add(tf.mul(tf.randomNormal([ 100, 2 ]), tf.tensor([ 2, 2 ])), tf.tensor([ 10, 0 ]));
+const clust3 = tf.add(tf.mul(tf.randomNormal([ 100, 2 ]), tf.tensor([ 2, 2 ])), tf.tensor([ -10, 0 ]));
 const clusData = tf.concat([ clust1, clust2, clust3 ]);
 
+const checkPredTrueCnt = (predClus: Tensor) => {
+  return tf.max(tf.stack([tf.sum(tf.equal(predClus, 0)), tf.sum(tf.equal(predClus, 1)), tf.sum(tf.equal(predClus, 2))])).dataSync()[0];
+}
+
+const checkClusAccuracy = async (predClus: Tensor) => {
+  const predClus1 = tf.slice(predClus, 0, 100);
+  const predClus2 = tf.slice(predClus, 100, 100);
+  const predClus3 = tf.slice(predClus, 200, 100);
+  const predTrue1 = checkPredTrueCnt(predClus1);
+  const predTrue2 = checkPredTrueCnt(predClus2);
+  const predTrue3 = checkPredTrueCnt(predClus3);
+  return (predTrue1 + predTrue2 + predTrue3) * 1.00 / 300.0;
+}
+
 describe('KMeans', () => {
-  // it('train clust', async() => {
-  //   const kmeans = new KMeans({ nClusters: 3 });
-  //   await kmeans.fit(clusData);
-  //   const predClus = await kmeans.predict(clusData);
-  //   predClus.print();
-  // });
-  it('train clust on batch', async() => {
+  it('train clust', async () => {
+    const kmeans = new KMeans({ nClusters: 3 });
+    await kmeans.fit(clusData);
+    const predClus = await kmeans.predict(clusData);
+    const accuracy = await checkClusAccuracy(predClus);
+    console.log('accuracy:', accuracy);
+    assert.isTrue(accuracy > 0.9);
+  });
+  it('train clust on batch', async () => {
     const kmeans = new KMeans({ nClusters: 3 });
     const batchSize = 30;
     const epochSize = Math.floor(clusData.shape[0] / batchSize);
@@ -27,9 +42,20 @@ describe('KMeans', () => {
       const batchX = tf.slice(clusData, [j * batchSize, 0], [batchSize ,2]);
       await kmeans.trainOnBatch(batchX);
     }
-    kmeans.centroids.print();
     const predClus = await kmeans.predict(clusData);
-    const predClusData = predClus.dataSync();
-    predClus.print();
+    const accuracy = await checkClusAccuracy(predClus);
+    console.log('accuracy:', accuracy);
+    assert.isTrue(accuracy > 0.9);
   });
+  it('save and load model', async () => {
+    const kmeans = new KMeans({ nClusters: 3 });
+    await kmeans.fit(clusData);
+    const modelJSON = await kmeans.toJson();
+    const kmeans2 = new KMeans({ nClusters: 3 });
+    kmeans2.fromJson(modelJSON);
+    const predClus = await kmeans2.predict(clusData);
+    const accuracy = await checkClusAccuracy(predClus);
+    console.log('accuracy:', accuracy);
+    assert.isTrue(accuracy > 0.9);
+  })
 });
