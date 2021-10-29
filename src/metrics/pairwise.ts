e8b0c52778e947e3273ac4c1ef638a2b070ea6db
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2021-10-29 16:51:13 +0800

    remove distance

--- src/metrics/pairwise.ts
+++
@@ -1,9 +0,0 @@
-import { Tensor, sub, norm } from "@tensorflow/tfjs-core";
-import { shapeEqual } from "../linalg";
-
-export const euclideanDistance = (x: Tensor, y: Tensor): number => {
-  if (!shapeEqual(x, y)) {
-    throw new TypeError('Shape of input tensor should be equal');
-  }
-  return norm(sub(x, y)).dataSync()[0];
-};
