4a9a83e35842dd5c2288c68da39a5e891b2bfe05
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2021-11-12 16:56:40 +0800

    gamma beta and t and normal

---
+++ src/math/utils.ts
@@ -0,0 +1,12 @@
+/**
+ * Check if two numbers are equal in a given tolerence.
+ * @param a input number a
+ * @param b input number b
+ * @param tol tolerence, default is 0
+ * @returns If a and b are equal in tolerence `tol`.
+ */
+export const numEqual = (a: number, b: number, tol = 0): boolean => {
+  if (a !== Infinity)
+    return Math.abs(a - b) < tol;
+  return a === b;
+};
