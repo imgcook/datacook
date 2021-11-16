16aae9df4acef4ee4d8218d453241c38bf921141
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2021-11-16 10:59:15 +0800

    linear regression analysis

---
+++ src/model/stat/linear-regression-analysis.ts
@@ -0,0 +1,177 @@
+import { Tensor, RecursiveArray, losses, squeeze, tensor, stack, transpose, matMul, slice, concat, ones, Tensor1D, reshape } from '@tensorflow/tfjs-core';
+import { layers, sequential, Sequential } from '@tensorflow/tfjs-layers';
+import { BaseEstimater } from '../base';
+import { checkArray } from '../../utils/validation';
+import { inverse } from '../../linalg';
+import { getResidualVariance, getRSquare, getAdjustedRSquare } from '../../metrics/regression'; 
+import { readFileSync } from 'fs';
+import { cdf } from '../../stat/t';
+
+/**
+ * Parameters for linear regression
+ */
+export interface LinearRegressionParams {
+  /**
+   * Whether to calculate the intercept for this model. If set to False, no intercept will be used in calculations
+   */
+  fitIntercept?: boolean,
+  /**
+   * This parameter is ignored when fit_intercept is set to False. If True, the regressors X will be normalized before regression by subtracting the mean and dividing by the l2-norm.
+   */
+  normalize?: boolean
+}
+
+export interface LinearRegerssionTrainParams {
+  tol?: number,
+  batchSize?: number,
+  epochs?: number,
+}
+
+export type SignificanceCode = '***' | '**' | '*' | '.' | ' ';
+
+export interface CoefficientSummary {
+  name: string,
+  estimate: number,
+  standardError: number,
+  tValue: number,
+  pValue: number,
+  significance: SignificanceCode
+}
+
+/**
+ * Get significance code according to p value
+ * @param pValue p-value
+ */
+export const getSignificanceCode = (pValue: number): SignificanceCode => {
+  if (pValue < 0.001) {
+    return '***';
+  }
+  if (pValue < 0.01) {
+    return '**';
+  }
+  if (pValue < 0.05) {
+    return '*';
+  }
+  if (pValue < 0.1) {
+    return '.';
+  }
+  return ' ';
+};
+
+/**
+ * Linear regression model
+ * LinearRegression fits a linear model with coefficients w = (w1, …, wp)
+ * to minimize the residual sum of squares between the observed targets in
+ * the dataset, and the targets predicted by the linear approximation.
+ */
+export class LinearRegressionAnalysis extends BaseEstimater {
+  public fitIntercept: boolean;
+  public normalize: boolean;
+  private featureSize: number;
+  public coefficients: Tensor;
+  public varianceBase: Tensor;
+  public nData: number;
+  public residualVariance: number;
+  public rSquare: number;
+  public adjustedRSquare: number;
+  /**
+   * Construction function of linear regression model
+   * @param params LinearRegressionParams
+   *
+   * Options in params
+   * ------------
+   *
+   * `fitIntercept`: Whether to calculate the intercept for this model. If set to False,
+   * no intercept will be used in calculations
+   *
+   * `normalize`: This parameter is ignored when fit_intercept is set to False. If True,
+   * the regressors X will be normalized before regression by subtracting the mean and dividing by the l2-norm.
+   *
+   * `optimizerType`: optimizer types for training. All of the following optimizers types supported in tensorflow.js
+   *  (https://js.tensorflow.org/api/latest/#Training-Optimizers) can be applied. Default to 'adam':
+   *  - 'sgd': stochastic optimizer
+   *  - 'momentum': momentum optimizer
+   *  - 'adagrad': adagrad optimizer
+   *  - 'adadelta': adadelta optimizer
+   *  - 'adam': adam optimizer
+   *  - 'adamax': adamax optimizer
+   *  - 'rmsprop': rmsprop optimizer
+   *
+   * `optimizerProps`: parameters used to init corresponding optimizer, you can refer to documentations in tensorflow.js
+   *  (https://js.tensorflow.org/api/latest/#Training-Optimizers) to find the supported initailization paratemters for a
+   *  given type of optimizer. For example, `{ learningRate: 0.1, beta1: 0.1, beta2: 0.2, epsilon: 0.1 }` could be used
+   *  to initialize adam optimizer.
+   */
+  constructor(params: LinearRegressionParams = {}) {
+    super();
+    this.fitIntercept = params.fitIntercept !== false;
+    this.normalize = params.normalize;
+  }
+
+  /** Fit linear regression model according to X, y. Here we use adam algorithm (a popular gradient-based optimization algorithm) for paramter estimation.
+   * @param xData Tensor like of shape (n_samples, n_features), input feature
+   * @param yData Tensor like of shape (n_sample, ), input target values
+   * @param params batchSize: batch size: default to 32, maxIterTimes: max iteration times, default to 20000
+   * @returns classifier itself
+   */
+  public async fit(xData: Tensor | RecursiveArray<number>,
+    yData: Tensor | RecursiveArray<number>,
+    params: LinearRegerssionTrainParams = { batchSize: 32, epochs: -1 }): Promise<LinearRegressionAnalysis> {
+  
+    const { x, y } = this.validateData(xData, yData);
+    const nFeature = x.shape[1];
+    const nData = x.shape[0];
+    const xTensor = concat([ ones([ nData, 1 ]), x ], 1);
+    const yTensor = y as Tensor1D;
+    const varianceBase = await inverse(matMul(transpose(xTensor), xTensor));
+    const coefs = squeeze(matMul(matMul(varianceBase, transpose(xTensor)), reshape(yTensor, [ -1, 1 ])));
+    const predY = squeeze(matMul(xTensor, reshape(coefs, [ -1, 1 ]))) as Tensor1D;
+    const residualVariance = getResidualVariance(yTensor, predY, nFeature + 1);
+    this.varianceBase = varianceBase;
+    this.coefficients = coefs;
+    this.featureSize = nFeature;
+    this.residualVariance = residualVariance;
+    this.nData = nData;
+    this.rSquare = getRSquare(yTensor, predY);
+    this.adjustedRSquare = getAdjustedRSquare(yTensor, predY, nFeature);
+    return this;
+  }
+
+  public printSummary(): void {
+    const summary = this.summary();
+    console.log('coefficients:');
+    console.table(summary.coefficients);
+    console.log('r-square:', summary.rSquare);
+    console.log('Adjusted r-square:', summary.adjustedRSquare);
+    console.log('Residual Standard Error', summary.residualStandardError);
+  }
+
+  public summary(): {
+    coefficients: Array<CoefficientSummary>, 
+    rSquare: number,
+    adjustedRSquare: number,
+    residualStandardError: number,
+    residualDegreeOfFreedom: number
+  } {
+    const df = this.nData - this.featureSize - 1;
+    const coefficients: Array<CoefficientSummary> = [];
+    for (let i = 0; i < this.featureSize + 1; i++) {
+      const estimate = slice(this.coefficients, i, 1).dataSync()[0];
+      const traceI = slice(this.varianceBase, [ i, i ], [ 1, 1 ]).dataSync()[0];
+      const standardError = Math.sqrt(this.residualVariance * traceI);
+      const tValue = estimate / standardError;
+      const cdfT = cdf(tValue, df);
+      const pValue = cdfT > 0.5 ? ((1 - cdfT) * 2) : (cdfT * 2);
+      const name = (i === 0) ? 'Intercept' : `coeff_${i}`;
+      const significance = getSignificanceCode(pValue);
+      coefficients.push({ name, estimate, standardError, tValue, pValue, significance });
+    }
+    return {
+      coefficients,
+      rSquare: this.rSquare,
+      adjustedRSquare: this.adjustedRSquare,
+      residualStandardError: Math.sqrt(this.residualVariance),
+      residualDegreeOfFreedom: this.nData - this.featureSize - 1
+    }
+  }
+}
---
+++ test/node/model/linear-regression-analysis.ts
@@ -0,0 +1,42 @@
+import { LinearRegressionAnalysis } from '../../../src/model/stat/linear-regression-analysis';
+import * as tf from '@tensorflow/tfjs-core';
+import { assert } from 'chai';
+
+const nData = 100;
+const cases = tf.mul(tf.randomNormal([ nData, 5 ]),[ 1, 10, 100, 2, 3 ]);
+const weight = tf.tensor([ 2, 3, 1, -4, 6 ]);
+const noise = tf.mul(tf.randomNormal([ nData ]), 2);
+const y = tf.add(tf.add(tf.sum(tf.mul(cases, weight), 1), 10), noise);
+
+const treesGrith = [ 8.3, 8.6, 8.8, 10.5, 10.7, 10.8, 11.0, 11.0, 11.1, 
+  11.2, 11.3, 11.4, 11.4, 11.7, 12.0, 12.9, 12.9, 13.3, 13.7, 13.8, 14.0, 14.2, 14.5,
+  16.0, 16.3, 17.3, 17.5, 17.9, 18.0, 18.0, 20.6 ];
+
+const treesHeight = [ 70, 65, 63, 72, 81, 83, 66, 75, 80, 75, 79, 76, 76, 
+  69, 75, 74, 85, 86, 71, 64, 78, 80, 74, 72, 77, 81, 82, 80, 80, 80, 87 ];
+
+const treesVolumn = [ 10.3, 10.3, 10.2, 16.4, 18.8, 19.7, 15.6, 18.2, 22.6,
+  19.9, 24.2, 21.0, 21.4, 21.3, 19.1, 22.2, 33.8, 27.4, 25.7, 24.9, 34.5, 31.7, 36.3,
+  38.3, 42.6, 55.4, 55.7, 58.3, 51.5, 51.0, 77.0 ]; 
+
+describe('Linear Regression', () => {
+
+  it('train simple dataset', async () => {
+    const lm = new LinearRegressionAnalysis({});
+    await lm.fit(cases, y); 
+    const summary = lm.summary();
+    console.log(summary);
+    
+  });
+
+  it('train on tree dataset', async () => {
+    const lm = new LinearRegressionAnalysis();
+    const treeFeatureTensor = tf.transpose(tf.tensor2d([ treesHeight, treesVolumn ]));
+    await lm.fit(treeFeatureTensor, treesGrith);
+    const summary = lm.summary();
+    console.table(summary.coefficientSummary);
+    console.log('r-square:', summary.rSquare);
+    console.log('Adjusted r-square:', summary.adjustedRSquare);
+    console.log('Residual Standard Error', summary.residualStandardError);
+  })
+});
