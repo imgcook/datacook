f7488af96709c0057fd7e472255ade1f3514050a
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2022-01-06 01:30:06 +0800

    fix introsort

--- src/model/tree/utils.ts
+++ src/model/tree/utils.ts
@@ -2,11 +2,74 @@ export const sort = () => {
   
 };
 
-export const introSort = () => {
+/**
+ * Median of three pivot selection, after Bentley and McIlroy (1993).
+ * @param values 
+ * @param n 
+ */
+export const median3 = (values: number[], start: number, end: number): number => {
+  const n = end - start + 1;
+  let a = values[start];
+  let b = values[start + Math.floor(n / 2)];
+  let c = values[end];
+  if (a < b) {
+    if (b < c) {
+      return b;
+    }
+    if (a < c) {
+      return c;
+    }
+    return a;
+  }
+  if (b < c) {
+    if (a < c) {
+      return a;
+    }
+    return c;
+  }
+  return b;
+};
 
+export const introSort = (values: number[], indicies: number[], start: number, end: number, maxDepth: number): void => {
+  let pivot: number;
+  let i = 0;
+  let l = 0;
+  let r = 0;
+  let n = end - start + 1;
+  while (n > 1) {
+    /**
+     * max depth limit exceeded ("gone quadratic")
+     */
+    if (maxDepth <= 0) {
+      heapSort(values, indicies, start, end);
+      return;
+    }
+    maxDepth -= 1
+    pivot = median3(values, start, end);
+    // tree-way partition
+    i = l = start;
+    r = end;
+    while (i < r) {
+      if (values[i] < pivot) {
+        swap(values, indicies, i, l);
+        i += 1;
+        l += 1;
+      } else {
+        if (values[i] > pivot) {
+          r -= 1;
+          swap(values, indicies, i, r);
+        } else {
+          i += 1;
+        }
+      }
+    }
+    introSort(values, indicies, start, l, maxDepth);
+    start = r;
+  }
 };
 
 export const swap = (values: number[], indices: number[], i: number, j: number): void => {
+  if (i === j) return;
   [values[i], values[j]] = [values[j], values[i]];
   [indices[i], indices[j]] = [indices[j], indices[i]];
 };
@@ -40,9 +103,10 @@ export const siftDown = (values: number[], indices: number[], start: number, end
   }
 };
 
-export const heapSort = (values: number[], indices: number[], n: number) => {
-  let start = (n - 2) / 2;
-  let end = n;
+export const heapSort = (values: number[], indices: number[], startInd: number, endInd: number) => {
+  const n = endInd - startInd + 1;
+  let start = startInd + (n - 2) / 2;
+  let end = endInd;
   // heapify
   while (true) {
     siftDown(values, indices, start, end);
@@ -51,10 +115,10 @@ export const heapSort = (values: number[], indices: number[], n: number) => {
     }
     start -= 1;
   }
-  end = n - 1
-  while (end > 0) {
-    swap(values, indices, 0, end);
-    siftDown(values, indices, 0 , end);
+  end = endInd - 1
+  while (end > start) {
+    swap(values, indices, start, end);
+    siftDown(values, indices, start , end);
     end = end - 1;
   }
 };
