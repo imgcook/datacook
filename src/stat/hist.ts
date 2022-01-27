e835217168a0cb5024c08c082969160456223012
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2022-01-27 13:08:23 +0800

    add test

--- src/stat/hist.ts
+++ src/stat/hist.ts
@@ -1,40 +1,50 @@
-import { checkArray } from '../utils/validation';
-import { Tensor, RecursiveArray, min, max, tidy, squeeze, dispose, Tensor1D } from '@tensorflow/tfjs-core';
-
-export interface HistData {
-  from: number;
-  to: number;
-  count: number;
-}
+import { Tensor1D } from "@tensorflow/tfjs-core";
 
 /**
- * Generate histogram Data especially for data visualization.
- * @param xData input data
- * @param bins split bin counts, default to 50
- * @param limitLeft limit value for left side
- * @param limitRight limit value for right side
+ * Get histogram data for given input
+ * @param xData inpur data of shape [nSamples, ]
+ * @param params parameters
+ * Option in params
+ * ----
+ * `bins`: number of cells, **default=50**
+ * `leftLim`: left limit to calculate, **default=min value of input data**
+ * `rightLim`: right limit to calculate, **default=max value of input data**
+ * @returns
+ * { steps; number[], counts: number[] }
+ * `steps`: array of giving the breakpoints between histogram cells,
+ * `counts`: array of number of data points in histogram cells
  */
-export const genHistData = async (xData: Tensor | RecursiveArray<number>, bins = 50,
-  limitLeft?: number, limitRight?: number): Promise<HistData[]> => {
-  const xTensor = tidy(() => squeeze(checkArray(xData, 'float32')) as Tensor1D);
-  const minVal = limitLeft ? limitLeft : tidy(() => min(xTensor).dataSync())[0];
-  const maxVal = limitRight ? limitRight : tidy(() => max(xTensor).dataSync())[0];
-  if (minVal > maxVal) {
-    throw new TypeError('Maximum value should be greater than minimum value');
+export const getHistData = (xData: Tensor1D | number[], params?: {
+    bins?: number,
+    leftLim?: number,
+    rightLim?: number
+  }): {
+    steps: number[],
+    counts: number[],
+  } => {
+  const xArray = checkJSArray(xData, 'float32', 1) as number[];
+  const nData = xArray.length;
+  const minVal = Math.min(...xArray);
+  const maxVal = Math.max(...xArray);
+  const { bins = 50, leftLim = minVal, rightLim = maxVal } = params;
+  if (rightLim < leftLim) {
+    throw new TypeError('Right limit should be greater than left limit');
   }
-  const xArray = await xTensor.array();
-  const histData = new Array(bins).fill(0);
-  const binStep = (maxVal - minVal) / bins;
-  for (let i = 0; i < xArray.length; i++) {
-    const histPos = Math.floor(xArray[i] / binStep);
-    histData[histPos] += 1;
+  const step = (rightLim - leftLim) / bins;
+  const steps = Array.from(new Array(bins).keys()).map((i) => leftLim + step * i);
+  const counts = new Array(bins).fill(0);
+  for (let i = 0; i < nData; i++) {
+    if (xArray[i] < leftLim || xArray[i] > rightLim) {
+      continue;
+    }
+    if (xArray[i] === rightLim) {
+      counts[ bins - 1 ] += 1;
+    } else {
+      counts[ Math.floor((xArray[i] - leftLim) / step) ] += 1;
+    }
   }
-  dispose(xTensor);
-  return histData.map((d: number, i: number) => {
-    return {
-      from: minVal + i * binStep,
-      to: minVal + (i + 1) * binStep - 1,
-      count: d
-    };
-  });
+  return {
+    steps,
+    counts
+  };
 };
