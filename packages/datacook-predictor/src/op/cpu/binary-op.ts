46e7781fe74c12a7cef4136b2f27321e8d2aba3c
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2022-04-01 23:48:40 +0800

    init predictor op

--- packages/datacook-predictor/src/op/cpu/binary-op.ts
+++ packages/datacook-predictor/src/op/cpu/binary-op.ts
@@ -1,9 +1,30 @@
 import { Matrix, Vector } from "../../classes";
-import { basicImplement2d, ByAxis } from "./basic-impl";
+import { basicImplement2dBinary, ByAxis } from "./basic-impl";
 
 export const add2d = (x: Matrix, y: Matrix | Vector | number, by: ByAxis = 0): Matrix => {
-  const addFunc = (a: number, b: number): number => {
+  const func = (a: number, b: number): number => {
     return a + b;
   };
-  return basicImplement2d(addFunc, x, y, by);
+  return basicImplement2dBinary(func, x, y, by);
+};
+
+export const sub2d = (x: Matrix, y: Matrix | Vector | number, by: ByAxis = 0): Matrix => {
+  const func = (a: number, b: number): number => {
+    return a - b;
+  };
+  return basicImplement2dBinary(func, x, y, by);
+};
+
+export const mul2d = (x: Matrix, y: Matrix | Vector | number, by: ByAxis = 0): Matrix => {
+  const func = (a: number, b: number): number => {
+    return a * b;
+  };
+  return basicImplement2dBinary(func, x, y, by);
+};
+
+export const div2d = (x: Matrix, y: Matrix | Vector | number, by: ByAxis = 0): Matrix => {
+  const func = (a: number, b: number): number => {
+    return a / b;
+  };
+  return basicImplement2dBinary(func, x, y, by);
 };
