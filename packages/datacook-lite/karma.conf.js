a6a08b6a32c074c9d1441ae41a2597f3fded7914
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2022-08-08 15:54:11 +0800

    fix build script

--- packages/datacook-lite/karma.conf.js
+++ packages/datacook-lite/karma.conf.js
@@ -3,17 +3,19 @@ const karmaTypescriptConfig = {
   // Disable coverage reports and instrumentation by default for tests
   coverageOptions: { instrumentation: false },
   reports: {},
-  bundlerOptions: { sourceMap: false }
+  bundlerOptions: {
+    sourceMap: false
+  }
 };
 
 module.exports = function(config) {
   config.set({
-
     frameworks: [ "mocha", "karma-typescript" ],
     karmaTypescriptConfig,
     files: [
       { pattern: 'test/setup.ts', type: 'js' },
       { pattern: "src/**/*.ts", type: 'js' },
+      { pattern: "src/*.ts", type: 'js' },
       { pattern: "test/**/*.ts", type: 'js' }
     ],
     preprocessors: {
