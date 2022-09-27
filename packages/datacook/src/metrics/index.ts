f4aab27deb374d01116d2fac7b91a72cd887f96d
sugarspectre <wangxiaojian.wxj@alibaba-inc.com>
2022-09-27 17:48:53 +0800

    metrics

--- packages/datacook/src/metrics/index.ts
+++ packages/datacook/src/metrics/index.ts
@@ -1,2 +1,12 @@
-export { accuracyScore } from './classifier';
+export {
+  accuracyScore,
+  getClassificationReport,
+  getConfusionMatrix,
+  getF1Score,
+  getPrecisionScore,
+  getPrecisionScores,
+  getF1Scores,
+  getRecallScore,
+  getRecallScores
+} from './classifier';
 export { getAdjustedRSquare, getMeanSquaredError, getResidualVariance, getAICLM, getRSquare } from './regression';
